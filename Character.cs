13ebe31cc54074656c0373be4804cf51f520b6b1
SuperCoolNinja <sbpro570@gmail.com>
2024-04-08 19:59:27 +0200

    init:

---
+++ Character.cs
@@ -0,0 +1,59 @@
+﻿namespace ClassOOP;
+
+internal class Character
+{
+    public string Name
+    {
+        get => _name ?? "default character";
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                _name = value;
+        }
+    }
+
+    public int Health
+    {
+        get => _health;
+        set
+        {
+            if (value > 0)
+                _health = value;
+        }
+    }
+
+    public int Force
+    {
+        get => _force;
+        set
+        {
+            if (value > 0)
+                _force = value;
+        }
+    }
+
+    public int Defense
+    {
+        get => _defense;
+        set
+        {
+            if (value > 0)
+                _defense = value;
+        }
+    }
+
+
+    public bool IsAlive() => _health > 0;
+    public void Attack(Character target)
+    {
+        target.Health -= Force - target.Defense;
+        Console.WriteLine($"{this.Name} attaque {target.Name}");
+    }
+
+
+
+    private string? _name;
+    private int _health = 100;
+    private int _force = 25;
+    private int _defense = 50;
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = new Character();
+            var ennemy = new Character();
+
+            character.Name = "Hero";
+            ennemy.Name = "Ennemy";
+
+
+            Console.WriteLine($"[{character.Name}] : Nombre de pv = {character.Health}");
+            Console.WriteLine($"[{ennemy.Name}] : Nombre de pv = {ennemy.Health}");
+
+            character.Attack(ennemy);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"[{character.Name}] : Nombre de pv = {character.Health}");
+            Console.WriteLine($"[{ennemy.Name}] : Nombre de pv = {ennemy.Health}");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            ennemy.Attack(character);
+
+
+            Console.WriteLine($"[{character.Name}] : Nombre de pv = {character.Health}");
+            Console.WriteLine($"[{ennemy.Name}] : Nombre de pv = {ennemy.Health}");
+        }
+    }
+}
